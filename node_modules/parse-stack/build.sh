41b8a2d3916f189a5915a29ccba1967adbd31b66
shubh <shahshubh1010@gmail.com>
2020-02-29 14:36:02 +0530

    cached all fix gitignore

--- node_modules/parse-stack/build.sh
+++
@@ -1,8 +0,0 @@
-rm -f lib/*
-coffee -cbo lib/ src/
-
-component build -s parse-stack -o . -n parse-stack
-
-coffee -cb src/ test/
-browserify test/*.js > test/browser/tests.js
-rm src/*.js test/*.js
