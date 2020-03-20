eb28da01249c6b4aeab743f15a1b166edc9b3ab8
Sakshi107 <46996938+Sakshi107@users.noreply.github.com>
2020-03-20 14:44:36 +0530

    updated edit.ejs new.ejs dashboard.ejs files

---
+++ public/javascripts/new.js
@@ -0,0 +1,4 @@
+$(".dropdown-menu li").click(function(){
+    var selText = $(this).text();
+    $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+  });
